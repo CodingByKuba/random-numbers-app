013ed717356d2f99c659d575ce5ecbdc987d979a
Jakub Marczak <19kuba92@gmail.com>
2023-09-15 11:18:54 +0200

    Added second loss type

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,37 @@
-import { useCallback, useState } from "react";
+import { useState } from "react";
 
 function App() {
+  const [lossType, setLossType] = useState<number>(0);
   const [firstNumbers, setFirstNumbers] = useState<number[]>([]);
   const [secondNumbers, setSecondNumbers] = useState<number[]>([]);
 
-  const getNumbers = useCallback(
-    (max: number, count: number) => {
-      let numbers: number[] = [];
-      let turn: number = 0;
-      while (turn < count) {
-        let generatedNumber: number = Math.floor(Math.random() * max + 1);
-        if (numbers.indexOf(generatedNumber) !== -1) continue;
-        numbers.push(generatedNumber);
-        turn++;
-      }
-      return numbers.sort((a, b) => a - b);
-    },
-    [firstNumbers, secondNumbers]
-  );
+  const getNumbers = (max: number, count: number) => {
+    let numbers: number[] = [];
+    let turn: number = 0;
+    while (turn < count) {
+      let generatedNumber: number = Math.floor(Math.random() * max + 1);
+      if (numbers.indexOf(generatedNumber) !== -1) continue;
+      numbers.push(generatedNumber);
+      turn++;
+    }
+    return numbers.sort((a, b) => a - b);
+  };
 
-  const handleClick = useCallback(() => {
-    setFirstNumbers(getNumbers(50, 5));
-    setSecondNumbers(getNumbers(12, 2));
-  }, [firstNumbers, secondNumbers]);
+  const handleClick = () => {
+    setFirstNumbers(getNumbers(lossType === 1 ? 42 : 50, 5));
+    setSecondNumbers(lossType === 1 ? [] : getNumbers(12, 2));
+  };
+
+  const handleSwitchLossType = () => {
+    setLossType((prev) => (!prev ? prev + 1 : 0));
+  };
 
   return (
     <>
       <h1>Losowanie:</h1>
+      <button onClick={handleSwitchLossType}>
+        Typ losowania: {lossType ? "Mini Lotto" : "Euro Jackpot"}
+      </button>
       <section>
         {firstNumbers.map((el: number, index: number) => (
           <article key={index}>{el}</article>
